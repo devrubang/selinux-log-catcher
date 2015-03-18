ed057d31728e5138655ecd0cadbc2b061a94038d
hyuk.kwon <kh31202@naver.com>
2015-03-18 11:16:27 +0900

    Add violation icon

--- SELinuxLogCatcher/gen/com/lge/selinuxlogcatcher/R.java
+++ SELinuxLogCatcher/gen/com/lge/selinuxlogcatcher/R.java
@@ -22,20 +22,22 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int penguin=0x7f020001;
+        public static final int deny=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int penguin=0x7f020002;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f08000a;
         public static final int imageView1=0x7f080002;
-        public static final int imageView2=0x7f080005;
+        public static final int imageView2=0x7f080006;
         public static final int imageView3=0x7f080004;
         public static final int logListView=0x7f080001;
-        public static final int logTextView=0x7f080008;
-        public static final int tagTextView=0x7f080007;
+        public static final int logTextView=0x7f080009;
+        public static final int tagTextView=0x7f080008;
         public static final int textView1=0x7f080003;
-        public static final int textView2=0x7f080006;
+        public static final int textView2=0x7f080007;
+        public static final int textView3=0x7f080005;
     }
     public static final class layout {
         public static final int activity_selinux_lcmain=0x7f030000;
